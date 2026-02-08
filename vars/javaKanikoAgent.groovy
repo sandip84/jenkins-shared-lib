63bbbb49b6e58d5fb54059a87a1fe6418bb2a6da
sandip84 <sandip84@hotmail.com>
2026-02-08 15:38:52 -0500

    added shared lib

---
+++ vars/javaKanikoAgent.groovy
@@ -0,0 +1,42 @@
+def call(Map config = [:], Closure body) {
+
+  def mavenImage  = config.mavenImage ?: "maven:3.9.6-eclipse-temurin-17"
+  def kanikoImage = config.kanikoImage ?: "gcr.io/kaniko-project/executor:debug"
+
+  podTemplate(yaml: """
+apiVersion: v1
+kind: Pod
+spec:
+  volumes:
+    - name: maven-settings
+      configMap:
+        name: maven-settings
+    - name: maven-repo-cache
+      emptyDir: {}
+
+  containers:
+  - name: maven
+    image: ${mavenImage}
+    command: ["cat"]
+    tty: true
+    volumeMounts:
+      - name: maven-settings
+        mountPath: /root/.m2/settings.xml
+        subPath: settings.xml
+      - name: maven-repo-cache
+        mountPath: /root/.m2/repository
+
+  - name: kaniko
+    image: ${kanikoImage}
+    command: ["sh","-c","cat"]
+    tty: true
+
+  - name: jnlp
+    image: jenkins/inbound-agent:latest
+""") {
+
+    node(POD_LABEL) {
+      body()
+    }
+  }
+}
